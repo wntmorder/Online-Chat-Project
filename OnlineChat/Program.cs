2a9fa2b7371a1c0aa6c2999bd7b89818ada60b48
Egor Korol <wntmorder@outlook.com>
2024-07-07 19:50:52 +0300

    refactor: Move encryption keys to appsettings.Development.json and update configuration - Moved encryption keys (Key and IV) from appsettings.json to appsettings.Development.json for enhanced security. - Updated Program.cs to load configuration settings based on the current environment. - Added validation in Program.cs to ensure encryption keys are not null or empty. - Updated encryption keys (Key and IV).

--- OnlineChat/Program.cs
+++ OnlineChat/Program.cs
@@ -23,13 +23,19 @@ builder.Services.AddDbContext<ChatDbContext>(options =>
 
 // Add connection configuration
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-                .AddCookie(options => //CookieAuthenticationOptions
+                .AddCookie(options => // CookieAuthenticationOptions
                     options.LoginPath = new PathString("/Account/Login"));
 
 // Read encryption settings from configuration
 IConfigurationSection encryptionSettings = builder.Configuration.GetSection("EncryptionSettings");
 string? key = encryptionSettings["Key"];
 string? iv = encryptionSettings["IV"];
+
+// Validate that key and IV are not null or empty
+if (string.IsNullOrEmpty(key) || string.IsNullOrEmpty(iv))
+{
+    throw new InvalidOperationException("Encryption settings (Key and IV) must be specified in the configuration.");
+}
 
 // Add PasswordService
 builder.Services.AddSingleton(new PasswordService(key, iv));
